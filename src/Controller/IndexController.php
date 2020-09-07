b6fbc512de3dce079722add6d81a2597fb947809
cristian16b <cristian.budzicz@gmail.com>
2020-09-07 09:58:43 -0300

    Subo cambios como mostrar en la twig el boton para redirigir

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,7 +9,7 @@ class IndexController extends AbstractController
     public function index()
     {
         // Agrega credenciales
-        MercadoPago\SDK::setAccessToken('');
+        MercadoPago\SDK::setAccessToken('TEST-6864113784926029-082523-64405d2ff4a697e4df1bedc147234d55-167188015');
 
 // Crea un objeto de preferencia
         $preference = new MercadoPago\Preference();
@@ -23,6 +23,12 @@ class IndexController extends AbstractController
         $preference->items = array($item);   
         $preference->save();
 
-        return $this->render('index.html.twig');
+        dump($preference);die;
+
+        return $this->render(
+            'index.html.twig',array(
+                'url' => $preference['preferenceinit_point']
+            )
+        );
     }
 }
